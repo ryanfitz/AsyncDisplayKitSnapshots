e2257cf8e0a4b32389ec14c6d131abba5067650e
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2017-01-24 07:17:54 -0500

    Adding quick and nimble for unit tests
     * created view model tests

---
+++ AsyncDisplayKitSnapshotsTests/ViewModelTests.swift
@@ -0,0 +1,38 @@
+//
+//  ViewModelTests.swift
+//  AsyncDisplayKitSnapshots
+//
+//  Created by Ryan Fitzgerald on 1/24/17.
+//  Copyright © 2017 Ryan Fitzgerald. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+@testable import AsyncDisplayKitSnapshots
+class ViewModelSpec: QuickSpec {
+  override func spec() {
+
+    var subject: ViewModel!
+
+    describe("username") {
+      it("is not valid when not set") {
+        subject = ViewModel()
+        expect(subject.isUsernameValid).to(beFalse())
+      }
+
+      it("is not valid when empty") {
+        subject = ViewModel()
+        subject.username = ""
+        expect(subject.isUsernameValid).to(beFalse())
+      }
+
+      it("is valid") {
+        subject = ViewModel()
+        subject.username = "foobar"
+        expect(subject.isUsernameValid).to(beTrue())
+      }
+    }
+  }
+}
